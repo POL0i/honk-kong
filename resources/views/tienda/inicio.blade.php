0612f863351cb668d1d0e53a48321ffb01acb7ed
wilyKamijou <wilianxd474@gmail.com>
2025-06-17 10:53:59 -0400

    bug and fuxes

--- resources/views/tienda/inicio.blade.php
+++ resources/views/tienda/inicio.blade.php
@@ -1,12 +1,3 @@
-
-@push('styles')
-    <link rel="stylesheet" href="{{ asset('css/banner.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/producto.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/actions.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/reseña.css') }}">
-     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-@endpush
 @extends('base')
 
 @push('styles')
